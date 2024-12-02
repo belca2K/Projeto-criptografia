94a9b25670af472bd01510c124049bee0d9c68c8
belc <belc@g5c46.cin.ufpe.br>
2024-12-02 14:58:51 -0300

    testing files func

--- test.c
+++ test.c
@@ -9,16 +9,20 @@ int main(){
 		printf("erro de abertura de arquivo!");
 		return(1);
 	}
-	
+
 	fseek(arq, 0, SEEK_END);
 	int tam = ftell(arq);
-	printf("%d\n", tam);
-	
-	char msg[tam];
-	fgets(msg, sizeof(msg), arq);
-	printf("mensagem:  %s\n", msg);
+//	printf("%d\n", tam);
+	fseek(arq, 0, SEEK_SET);
 
-fclose(arq);
+	char msg[tam+1];
+
+
+	fscanf(arq, "%[^\n]", &msg[0]);
+	fclose(arq);
+
+	printf("MENSAGEEEEEEEEEEEEEM: %s",msg);
+
 return 0;
 }
 
